d089b4b30a13be941af3c7fff8c5e59d96365ec5
gregpass <gregpass@quadrone.local>
2014-08-11 23:01:55 -0400

    Added program to generate random startups

---
+++ generate_startup.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# command line usage
+if ARGV.length != 3
+	puts "Usage: <startups filename> <use cases filename> <num startups>"
+	exit
+end
+
+# get command line options
+startups_filename = ARGV[0]
+use_cases_filename = ARGV[1]
+num_startups = ARGV[2]
+
+# read files
+startups = File.readlines(startups_filename)
+use_cases = File.readlines(use_cases_filename)
+
+# generate random new startups
+num_startups.to_i.times do
+	startup = startups[rand(startups.length)].chomp
+	use_case = use_cases[rand(use_cases.length)].chomp
+	puts startup + " of " + use_case
+end
